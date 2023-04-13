27b945c3eca79a7961eba435f4d27f5f7f10322c
linfeiwu <3211274226@qq.com>
2023-04-13 10:26:30 +0800

    update apitest0

--- pytest/practice05_apitest/HogwardsHttp/test_auth.py
+++ pytest/practice05_apitest/HogwardsHttp/test_auth.py
@@ -1,17 +1,17 @@
 import requests
 from requests.auth import HTTPBasicAuth
-def test_auth():
+'''def test_auth():
     r = requests.get(url="https://httpbin.testing-studio.com/basic-auth/banana/123",
                      auth=HTTPBasicAuth("banana","123"))
     print(r.text)
 def test_sms():
      header = {'X-Qiniu-Uid': '1810775223'}
      r = requests.get(url= "http://sms-portal-sms-env-dev.qa.qiniu.io/v1/signature",headers=header)
-     print(r.text)
+     print(r.text)'''
 def test_sms1():
      header = {
          'X-Qiniu-Uid': '1810775223',
-         'Authorization': "Qiniu QxZugR8TAhI38AiJ_cptTl3RbzLyca3t-AAiH-Hh:kxJVJBwA7u8rSC4Xw0BWEmJ6tjw=",
+         'Authorization': "Qiniu QxZugR8TAhI38AiJ_cptTl3RbzLyca3t-AAiH-Hh:rr0VKtHZQtjauaKGFzoj-jyHowk=",
          'Content-Type': "application/x-www-form-urlencoded"
          }
      
